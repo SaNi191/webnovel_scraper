6d95210b2f2024056ef383cc843c75ec25a1bb25
riseowo <everett.lin17@gmail.com>
2026-05-17 23:12:30 -0400

    mangadex csv

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import requests
 import csv
+import time
 
-response = requests.get("https://api.mangadex.org/manga?limit=10")
+MAX_OFFSET = 10000
 
-data = response.json()
+data = []
+offset = 0
 
-for manga in data["data"]:
-    for tag in manga["attributes"]["tags"]:
-        print(tag["attributes"]["name"]["en"])
+while offset < MAX_OFFSET:
+    response = requests.get(f"https://api.mangadex.org/manga?limit=100&offset={offset}")
+    manga = response.json()
+    data.extend(manga["data"])
+    offset += 100
+    time.sleep(0.2)
 
 with open("manga_tags.csv", "w", newline="", encoding="utf-8") as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(["Manga Title", "Tag Name"])
-    for i, manga in enumerate(data["data"]):
+    for manga in data:
         # Getting the English title of the manga, if possible
-        manga_title = manga["attributes"]["title"]
+        manga_title = list(manga["attributes"]["title"].values())[0]
         for titles in manga["attributes"]["altTitles"]:
             if "en" in titles:
-                manga_title = titles["en"]
+                manga_title = list(titles.values())[0]
                 break
 
         # Creating the list of tag names    
@@ -26,3 +31,5 @@ with open("manga_tags.csv", "w", newline="", encoding="utf-8") as csvfile:
                     for tag in manga["attributes"]["tags"]
                     ])
         csvwriter.writerow([manga_title, tag_names])
+
+print("Data has been written to manga_tags.csv")
